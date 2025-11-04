cc4f7aaaba4f8d7e6a3e3402ae71931fd6ed77e8
Jésus Pérez <jesus.perez.developer@gmail.com>
2025-11-04 15:10:33 +0100

    Merge branch 'feature/implement_supabase_auth'

--- lib/src/iautomat_auth_manager.dart
+++ lib/src/iautomat_auth_manager.dart
@@ -2,6 +2,8 @@ library;
 
 /// Barrel interno que exporta solo los elementos necesarios del paquete.
 export 'auth_repository.dart';
+export 'auth_repository_factory.dart';
 export 'firebase_auth_repository.dart';
 export 'models/user_model.dart';
 export 'result.dart';
+export 'supabase_auth_repository.dart';
